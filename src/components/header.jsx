4957f4ba6b2744a1528ec8dd36a8d47a1678a528
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-06-16 01:27:39 +0200

    feat: template added

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom"
+
+export default function Header() {
+  return (
+    <div className="w-full flex justify-center items-center bg-gray-100 bg-white">
+      <div className="content flex justify-between items-center gap-4 py-2">
+        <NavLink to="/" className="text-2xl font-bold mr-auto">
+          Venyu
+        </NavLink>
+        <NavLink to="/">
+          Anasayfa
+        </NavLink>
+        <NavLink to="/about" className="mr-4">
+          Hakkımızda
+        </NavLink>
+      </div>
+    </div>
+  )
+}
